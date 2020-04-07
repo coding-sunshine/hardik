18bec41cd80fd98288f2f7bf34ef2aa21ec720c9
Sunshine <sunshine.mahant@gmail.com>
2020-04-08 07:34:24 +1000

    UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+{{--        <script src="{{ asset('js/app.js') }}" defer></script>--}}
 
         <!-- Styles -->
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
